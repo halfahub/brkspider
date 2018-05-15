346f01b83841fca6ac9b08b3fd208f2a9f941c3a
Aleksandr Laburenko <laburenko.av@gmail.com>
2018-05-15 17:08:05 +0300

    added method stubs for GetMethod class

--- src/GetMethod.java
+++ src/GetMethod.java
@@ -1,4 +1,14 @@
 public class GetMethod implements HttpMethod {
     public GetMethod(String url) {
     }
+
+    @Override
+    public void releaseConnection() {
+
+    }
+
+    @Override
+    public String getResponseBodyAsString() {
+        return null;
+    }
 }
